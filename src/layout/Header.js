e9a6c5be240a6e0c8bf7f7ff2be410e581d9b790
Cheikhani <skibnewrim@gmail.com>
2020-09-10 11:53:42 -0700

    using material-ui to set up the header

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,7 +1,20 @@
 import React from "react"
+import MenuBar from "./MenuBar"
 
 const Header = () => {
-  return <h1>the header</h1>
+  return (
+    <header className="header">
+      {/* <ul>
+        <li>HOME</li>
+        <li>TO-DO LIST</li>
+        <li>NOTES</li>
+        <li>Bookmarks</li>
+        <li>Contact Keeper</li>
+      </ul> */}
+
+      <MenuBar />
+    </header>
+  )
 }
 
 export default Header
